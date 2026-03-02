6cbb3170880971111d3b1215aae72a5c9bb13749
Wowieee4 <evanwilliam303@gmail.com>
2026-03-02 07:31:49 +0700

    two sum comments

--- easy/0001_two_sum.py
+++ easy/0001_two_sum.py
@@ -2,15 +2,16 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         dictionary_history = {}
     
-        for i, current_num in enumerate(nums):
+        for current_index, current_num in enumerate(nums): # [2,7,11,15], target = 9
             
-            target_partner = target - current_num
+            target_partner = target - current_num # 9 - 2 = 7, 9 - 7 = 2, 9 - 11 = -2, 9 - 15 = -6
             
             if target_partner in dictionary_history:
                 partner_index = dictionary_history[target_partner]
             
-                return [partner_index, i]
+                return [partner_index, current_index]
     
-            dictionary_history[current_num] = i
+            dictionary_history[current_num] = current_index # [key] = value
             
         return []
+
